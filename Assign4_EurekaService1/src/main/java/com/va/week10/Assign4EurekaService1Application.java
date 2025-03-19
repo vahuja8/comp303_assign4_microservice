97040e7482dbf3016dc332f9a02b8230e8547142
vahuja8 <vahuja8@mycentennial.ca>
2025-03-19 11:45:55 -0400

    comp303_microservice_project

---
+++ Assign4_EurekaService1/src/main/java/com/va/week10/Assign4EurekaService1Application.java
@@ -0,0 +1,13 @@
+package com.va.week10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assign4EurekaService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assign4EurekaService1Application.class, args);
+	}
+
+}
